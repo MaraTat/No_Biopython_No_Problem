e009baeb3726da04483172a189c814a6dedc540a
Mara <61852141+MaraTat@users.noreply.github.com>
2020-08-08 20:13:06 +0100

    Create ChromUnzip.py

---
+++ ChromUnzip.py
@@ -0,0 +1,18 @@
+# Primary function to open the zipped chromosome file and extract the data 
+# returned as a list split per entry. 
+
+def ChromUnzip(arg):
+    """ This function accepts a single argument which is a zipped GenBank 
+        file stored on a local drive
+                
+        Input = zipped input file
+        Return = list of unzipped GenBank file to be used in downstream functions
+    """
+    try:
+        with gzip.open (arg, 'rt') as file:
+            genbank = file.read().split('//\n')
+            genbank = [x.replace('\n', '') for x in genbank]
+            genbank = genbank[:(len(genbank)-1):] # necessary because the end of the file contains an empty line
+            return genbank
+    except:
+        return print('Please enter a file in the form of filename.gz')
